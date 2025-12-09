219d7add508642f1c6d17e2e28216d649a62f2db
Anthony Herbert <anthony@prettyprinted.com>
2025-12-09 09:58:03 +0700

    initial commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+from fastapi import FastAPI
+from app.core.config import settings
+from datetime import datetime 
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    redis = aioredis.from_url(settings.REDIS_URL)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
+
+app = FastAPI(lifespan=lifespan) 
+
+@app.get("/cached") 
+@cache(expire=120)
+def index(response: Response):
+    return {"message": f"Last generated at {datetime.now()}"}
